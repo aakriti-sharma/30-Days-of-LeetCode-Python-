a3c22129e46f9b7ea021ff10e3097a5bc83d747f
Aakriti Sharma <43930106+aakriti-sharma@users.noreply.github.com>
2021-10-18 23:35:20 +0530

    D 18 Q 1

---
+++ Day 18/Intersection of Two Arrays.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        l=[]
+        for i in nums1:
+            if i in nums2:
+                l.append(i)
+                nums2.remove(i)
+        return l
+        
